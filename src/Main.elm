a83da917b615e0f49c6ff5785ab9cb3e523dfb38
Tom Bradley <bradley.tom.r@gmail.com>
2018-11-20 11:53:14 +0000

    Add basic JSON decoder

--- src/Main.elm
+++ src/Main.elm
@@ -7,6 +7,8 @@ import Element.Events as Events
 import Element.Input as Input
 import File exposing (File)
 import File.Select as Select
+import Html exposing (Html)
+import Json.Decode exposing (Decoder, field, map2, string)
 import Task
 
 
@@ -19,38 +21,51 @@ type alias Flags =
 
 
 type alias Model =
-    { warehouses : String }
+    { warehouses : Warehouse }
+
+
+type alias Warehouse =
+    { id : String
+    , name : String
+
+    -- , location : String
+    -- , lat : Float
+    -- , lon : Float
+    -- , rating : Float
+    -- , temperature : String
+    -- , capacity : Int
+    }
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( Model "", Cmd.none )
+    let
+        model =
+            { warehouses = { id = "", name = "" } }
+    in
+    ( model, Cmd.none )
 
 
 
 -- VIEW
 
 
-view : Model -> Browser.Document Msg
+view : Model -> Html Msg
 view model =
-    { title = "Warehouses"
-    , body =
-        [ Element.layout [ width fill ] <|
-            column []
-                [ el [ alignTop ] <|
-                    Input.button
-                        [ Border.color <| rgb255 0 0 0
-                        , Border.width 2
-                        , Border.rounded 2
-                        , padding 5
-                        ]
-                        { label = text "Load Warehouse CSV"
-                        , onPress = Just CsvRequested
-                        }
-                , el [] <| text model.warehouses
-                ]
-        ]
-    }
+    Element.layout [ width fill ] <|
+        column []
+            [ el [ alignTop ] <|
+                Input.button
+                    [ Border.color <| rgb255 0 0 0
+                    , Border.width 2
+                    , Border.rounded 2
+                    , padding 5
+                    ]
+                    { label = text "Load Warehouse JSON"
+                    , onPress = Just JsonRequested
+                    }
+            , el [] <| text model.warehouses.name
+            ]
 
 
 
@@ -58,32 +73,49 @@ view model =
 
 
 type Msg
-    = CsvRequested
-    | CsvLoaded File
-    | CsvParsed String
+    = JsonRequested
+    | JsonLoaded File
+    | JsonParsed String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        CsvRequested ->
-            ( model, requestCsv )
+        JsonRequested ->
+            ( model, requestJson )
 
-        CsvLoaded file ->
-            ( model, loadCsv file )
+        JsonLoaded json ->
+            ( model, loadJson json )
 
-        CsvParsed warehousesString ->
-            ( { model | warehouses = warehousesString }, Cmd.none )
-
-
-requestCsv : Cmd Msg
-requestCsv =
-    Select.file [ "text/csv" ] CsvLoaded
+        JsonParsed warehouses ->
+            ( { model | warehouses = decodeJson warehouses }, Cmd.none )
 
 
-loadCsv : File -> Cmd Msg
-loadCsv file =
-    Task.perform CsvParsed (File.toString file)
+requestJson : Cmd Msg
+requestJson =
+    Select.file [ "application/json" ] JsonLoaded
+
+
+loadJson : File -> Cmd Msg
+loadJson json =
+    Task.perform JsonParsed <| File.toString json
+
+
+decodeJson : String -> Warehouse
+decodeJson warehouses =
+    case Json.Decode.decodeString decodeWarehouse warehouses of
+        Ok result ->
+            result
+
+        Err error ->
+            { id = "", name = "" }
+
+
+decodeWarehouse : Decoder Warehouse
+decodeWarehouse =
+    map2 Warehouse
+        (field "id" string)
+        (field "name" string)
 
 
 
@@ -92,7 +124,7 @@ loadCsv file =
 
 main : Program Flags Model Msg
 main =
-    Browser.document
+    Browser.element
         { init = init
         , view = view
         , update = update
